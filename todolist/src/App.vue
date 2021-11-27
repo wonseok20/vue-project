a77b6d8ab0520966982e0ac4632e21162f369381
wonseok20 <economic21@naver.com>
2021-11-27 19:42:24 +0900

    todolist-1day update
    todolist 1day update

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+      import TodoHeader from './components/TodoFooter.vue'
+      import TodoFooter from './components/TodoFooter.vue'
+      import TodoInput from './components/TodoInput.vue'
+      import TodoList from './components/TodoList.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    export default {
+      components: {
+        'TodoHeader': TodoHeader,
+        'TodoInput': TodoInput,
+        'TodoList' : TodoList,
+        'TodoFooter' : TodoFooter
+      }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  
 </style>
