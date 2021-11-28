fd9052add2f34b0a9556338e84ca9de22869e35b
wonseok20 <economic21@naver.com>
2021-11-29 02:53:31 +0900

    final upload
    final upload

--- todolist/src/components/TodoFooter.vue
+++ todolist/src/components/TodoFooter.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        footer
+    <div class="clearAllContainer">
+        <span class="clearAllBtn" @click="clearTodo">Clear All</span>
     </div>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        clearTodo(){
+           this.$emit('removeAll');
+        }
+    }
 }
 </script>
 
-<style>
-    
+<style scoped>
+    .clearAllContainer{
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background-color: white;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+    .clearAllBtm{
+        color: #e20303;
+        display: block;
+    }
 </style>
